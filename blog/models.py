7f12145559ef7b933624b788bf8dfb15fe0632e6
KitLavis <>
2023-12-31 11:35:23 +0000

    add basic PostList view

--- blog/models.py
+++ blog/models.py
@@ -52,6 +52,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.content} by {self.name}"
-    
+
     def number_of_likes(self):
         return self.likes.count()
