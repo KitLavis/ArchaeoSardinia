0ddb743c63c70d2523215989eea80adcc87dd4a0
KitLavis <>
2024-01-05 11:25:46 +0000

    Custom post_detail function started

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ('.body',)
+        fields = ('.content',)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Post
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -9,6 +12,9 @@ class PostList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 6
 
-class post_detail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+class post_detail(request, slug):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.count()
+    if request.method == "POST"
