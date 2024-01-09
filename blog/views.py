f64ca4c9f4fa058ded14db6716cf11a41cb8ae5b
Kit Lavis <kcjlavis@gmail.com>
2024-01-09 16:18:27 +0000

    Add user comment functionality

--- blog/views.py
+++ blog/views.py
@@ -31,3 +31,29 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+    
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("-created_on")
+        
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form
+            },
+        )
