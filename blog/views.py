50b074444bb6e709ef476e04c78245ac3b91a1f4
Kit Lavis <kcjlavis@gmail.com>
2024-01-10 14:27:11 +0000

    Comment CRUD complete. Temp styling and layout

--- blog/views.py
+++ blog/views.py
@@ -65,12 +65,12 @@ class PostDetail(View):
             },
         )
 
-def comment_edit(self, request, slug, comment_id, *args, **kwargs):
+def comment_edit(request, slug, comment_id):
     if request.method == "POST":
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comment = get_object_or_404(Comment, pk=comment_id)
-        comment_form = CommentForm(data=request.Post, instance=comment)
+        comment_form = CommentForm(data=request.POST, instance=comment)
     
     if comment_form.is_valid() and comment.name == request.user:
         comment = comment_form.save(commit=False)
@@ -78,6 +78,20 @@ def comment_edit(self, request, slug, comment_id, *args, **kwargs):
         messages.add_message(request, messages.SUCCESS, 'Comment successfully updated!')
     else:
         messages.add_message(request, messages.ERROR, 'Error updating comment! Please try again.')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.name == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
