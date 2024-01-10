ad1e2a254cdeee2514c65548516907b70d898b68
Kit Lavis <kcjlavis@gmail.com>
2024-01-10 10:26:53 +0000

    Dployment attempt two

--- insightssardinia/settings.py
+++ insightssardinia/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-kitlavis-insightssardin-36xckim49yh.ws-eu107.gitpod.io', 'insights-sardinia.herokuapp.com']
+ALLOWED_HOSTS = ['8000-kitlavis-insightssardin-36xckim49yh.ws-eu107.gitpod.io', '.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-kitlavis-insightssardin-36xckim49yh.ws-eu107.gitpod.io']
 
