cc6877acf2416e728148673fe762e22ec695e8f2
Kit Lavis <kcjlavis@gmail.com>
2024-01-10 11:08:39 +0000

    Add Like class view

--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,9 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
         return render(
             request,
@@ -27,7 +30,7 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": False,
-                # "liked": liked,
+                "liked": liked,
                 "comment_form": CommentForm()
             },
         )
@@ -36,6 +39,9 @@ class PostDetail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
         comment_form = CommentForm(data=request.POST)
 
@@ -54,6 +60,19 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": True,
-                "comment_form": comment_form
+                "comment_form": comment_form,
+                "liked": liked
             },
         )
+
+
+class Like(View):
+
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        
+        return HTTPResponseRedirect(reverse('post_detail', args=[slug]))
