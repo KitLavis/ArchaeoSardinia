2fda71c6f19353b871e48854a98dc959baae681e
Kit Lavis <kcjlavis@gmail.com>
2024-01-11 17:07:33 +0000

    Comments layout and styling updated

--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,8 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.order_by("-created_on")
+        comments = post.comments.order_by("created_on")
+        comment_count = post.comments.count()
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -31,6 +32,7 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "comment_count": comment_count,
                 "commented": False,
                 "liked": liked,
                 "comment_form": comment_form
@@ -40,7 +42,7 @@ class PostDetail(View):
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.order_by("-created_on")
+        comments = post.comments.order_by("created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
