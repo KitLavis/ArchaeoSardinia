ae16f55e07ac0d928c0168a3c37837ad7e676108
Kit Lavis <kcjlavis@gmail.com>
2024-01-12 15:42:07 +0000

    contributor cards added

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,7 @@
-from . import views
+from about.views import TeamList
 from django.urls import path
 
 urlpatterns = [
     # path('our-goal', views.OurGoals.as_view(), name='our-goals'),
-    path('the-team', views.TeamList.as_view(), name='meet-the-team'),
+    path('the-team', TeamList.as_view(), name='meet-the-team'),
 ]
