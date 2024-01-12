b9b16bd2788c7057caa38e7f09461a02ce8abf33
Kit Lavis <kcjlavis@gmail.com>
2024-01-12 11:01:33 +0000

    ContributorAdmin added and models migrated

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contributor
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Contributor)
+class ContributorAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'role', 'created_on')
+    search_fields = ['name', 'role']
+    summernote_fields = ('content')
