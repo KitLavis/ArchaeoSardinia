f35a8e15fc6253673dfc5956e47b1cc929f31caf
Kit Lavis <kcjlavis@gmail.com>
2024-01-12 10:27:55 +0000

    Contributor model created

--- about/models.py
+++ about/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Contributor(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    role = models.CharField(max_length=200)
+    headshot = CloudinaryField('image', default='placeholder')
+    background = models.TextField()
+    motivation = models.TextField()
+    fav_place = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
