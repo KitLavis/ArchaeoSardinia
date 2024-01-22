b5cbea9d45e1c1bcc3e99396686e74dd57bde001
Kit Lavis <kcjlavis@gmail.com>
2024-01-22 16:31:44 +0000

    Add comments/docstrings to JS and CSS

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,3 +1,8 @@
+/**
+ * Adds edit and delete functionality to comments
+ * Original code from the Code Institute Django blog
+ * https://github.com/Code-Institute-Solutions/blog/blob/main/15_testing/static/js/comments.js
+ */
 const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_content");
 const commentForm = document.getElementById("commentForm");
@@ -7,7 +12,13 @@ const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
-
+/**
+ * Adds edit functionality to pencil icons
+ * On click it recieves the respective comment's ID
+ * Populates the comment content area with current content
+ * Changes submit button to say update
+ * Sets the form's action to `edit_comment/${commentId}`
+ */
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
         let commentId = e.target.getAttribute("comment_id");
@@ -18,6 +29,12 @@ for (let button of editButtons) {
     });
 }
 
+/**
+ * Adds delete functionality to dustbin icons
+ * On click it recieves the respective comment's ID
+ * Changes deleteConfirm href to respective comments deletion endpoint
+ * Displays the delete confirmation modal
+ */
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         let commentId = e.target.getAttribute("comment_id");
